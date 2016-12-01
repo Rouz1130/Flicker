9c13b29e9c892509d5774b4b2c8a3667f491d37f
Jonathan Buchner <websites@jonathanbuchner.com>
2016-11-30 17:56:22 -0800

    Final commit for the day

--- src/EpicPhotoShare/Controllers/HomeController.cs
+++ src/EpicPhotoShare/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using EpicPhotoShare.Models;
+using EpicPhotoShare.ViewModels;
 
 
 
@@ -15,6 +16,7 @@ namespace EpicPhotoShare.Controllers
         private readonly ApplicationDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private IEnumerable<IdentityError> myErrors { get; set; }
 
         // dependency injection in the construtor to configure our services/
         public HomeController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
@@ -33,6 +35,24 @@ namespace EpicPhotoShare.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var user = new ApplicationUser { UserName = model.Email };
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return  View();
+            }
+        }
+        public IActionResult Errors()
+        {
+            return View(myErrors);
         }
     }
 }
